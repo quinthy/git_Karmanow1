9ddfb18536f29aa9e96dac622c0915eba26cdc9d
quinthy <90686981+quinthy@users.noreply.github.com>
2021-12-16 15:44:00 +0300

    Add files via upload

--- Kon/var2.php
+++ Kon/var2.php
@@ -1,8 +1,66 @@
 <?php
 require_once 'connect.php';
 ?>
-<form action="var.php" method="GET">
-Приз:<input type="text" name="klichka"><br>
+<form action="var2.php" method="GET">
+Выберет кличку живтоного:<SELECT name = "klichka">
+<?php
+$result = mysqli_query($link,"SELECT
+  klichka.klichka,
+  jivotnoe.prizi
+FROM jivotnoe
+  INNER JOIN pizo
+    ON jivotnoe.prizi = pizo.id_prizi
+  INNER JOIN klichka
+    ON jivotnoe.klichka = klichka.id_klichka
+  INNER JOIN trener
+    ON trener.klichka_jovotnogo = klichka.id_klichka");
+$rows = mysqli_fetch_all($result,MYSQLI_ASSOC);
+foreach ($rows as $row)
+{
+echo "<option>". ($row['klichka']."</option>");
+}
+?>
+</select>
 <input type="submit" name="submit" value="Поиск"><br>	
 </form>
-<input type="submit" value="назад" onclick="location.href='var.php';" />
+<?php
+if ($_GET['submit'])
+{
+	$result=mysqli_query($link,"SELECT
+  trener.fam,
+  trener.ima,
+  trener.otch,
+  klichka.klichka
+FROM trener
+  INNER JOIN klichka
+    ON trener.klichka_jovotnogo = klichka.id_klichka
+  INNER JOIN jivotnoe
+    ON jivotnoe.klichka = klichka.id_klichka
+WHERE klichka.klichka = '$_GET[klichka]'");			
+$rows=mysqli_fetch_all($result,MYSQLI_ASSOC);
+echo '<h4><caption>'."ФИО тренера животного " . $_GET['priz'] .'</caption></h4>';
+echo '<table border="1">';
+echo '<tr>';
+echo '<th>'."Фамилия".'</th>';
+echo '<th>'."имя".'</th>';
+echo '<th>'."отчестов".'</th>';
+echo '<th>'."кличка животного".'</th>';
+echo '</tr>';
+foreach ($rows as $row)
+{
+echo '<tr>';
+echo '<td>'.$row['fam'].'</td>';
+echo '<td>'.$row['ima'].'</td>';
+echo '<td>'.$row['otch'].'</td>';
+echo '<td>'.$row['klichka'].'</td>';
+echo '</tr>';
+}
+}
+echo '</table>';
+?>
+<br><input type="submit" value="запрос 1" onclick="location.href='var.php';" /></br>
+<br><input type="submit" value="запрос 3" onclick="location.href='var3.php';" /></br>
+<br><input type="submit" value="запрос 4" onclick="location.href='var4.php';" /></br>
+<br><input type="submit" value="запрос 5" onclick="location.href='var5.php';" /></br>
+
+
--- Kon/var4.php
+++ Kon/var4.php
@@ -1,4 +1,53 @@
 <?php
 require_once 'connect.php';
 ?>
-<input type="submit" value="назад" onclick="location.href='var.php';" />
+<form action="var4.php" method="GET">
+Выберете наличие родословной(<SELECT name = "klichka">
+<?php
+$result = mysqli_query($link,"SELECT
+ sor.rod
+FROM sor");
+$rows = mysqli_fetch_all($result,MYSQLI_ASSOC);
+foreach ($rows as $row)
+{
+echo "<option>". ($row['rod']."</option>");
+}
+?>
+</select>
+<input type="submit" name="submit" value="Поиск"><br>	
+</form>
+<?php
+if ($_GET['submit'])
+{
+	$result=mysqli_query($link,"SELECT
+  sor.rod,
+  klichka.klichka
+FROM jivotnoe
+  INNER JOIN sor
+    ON jivotnoe.nal_rodoslovnoi = sor.id_sor
+  INNER JOIN klichka
+    ON jivotnoe.klichka = klichka.id_klichka
+WHERE sor.rod= '$_GET[klichka]'");			
+$rows=mysqli_fetch_all($result,MYSQLI_ASSOC);
+echo '<h4><caption>'."кличка животного, наличие родословной" . $_GET['priz'] .'</caption></h4>';
+echo '<table border="1">';
+echo '<tr>';
+echo '<th>'."кличка животного".'</th>';
+echo '<th>'."наличие родословной".'</th>';
+echo '</tr>';
+foreach ($rows as $row)
+{
+echo '<tr>';
+echo '<td>'.$row['klichka'].'</td>';
+echo '<td>'.$row['rod'].'</td>';
+echo '</tr>';
+}
+}
+echo '</table>';
+?>
+<br><input type="submit" value="запрос 1" onclick="location.href='var.php';" /></br>
+<br><input type="submit" value="запрос 2" onclick="location.href='var2.php';" /></br>
+<br><input type="submit" value="запрос 3" onclick="location.href='var3.php';" /></br>
+<br><input type="submit" value="запрос 5" onclick="location.href='var5.php';" /></br>
+
+
